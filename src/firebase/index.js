7f87cab1b3427b80483dcaf52a4b892f53b5d3b1
João Guilherme <joaaogui@gmail.com>
2020-05-22 01:16:58 -0300

    Adds type of user modal

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDS8wHGKx27QEaIAvtlcFV_csHFoOd9tj4',
+  authDomain: 'gestantesxcoronavirus.firebaseapp.com',
+  databaseURL: 'https://gestantesxcoronavirus.firebaseio.com',
+  projectId: 'gestantesxcoronavirus',
+  storageBucket: 'gestantesxcoronavirus.appspot.com',
+  messagingSenderId: '238866103271',
+  appId: '1:238866103271:web:12cb7c5ba0d6a3ae451b42',
+  measurementId: 'G-W8QMWDP4ER'
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
+export default db
